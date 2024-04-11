3fba7796d657c6d78442c1dce4ffc937b6a9cbfd
Shanmuganand <12005921@bluetriton.com>
2024-04-11 13:05:08 +0530

    duplicate images deleted, api route for contact page added

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,17 +1,50 @@
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import classes from './post-content.module.css';
 import PostHeader from './post-header';
-
+import Image from 'next/image';
 
 export default function PostContent({ post }) {
   const { slug, image, content, title } = post;
   const imagePath = `/images/posts/${slug}/${image}`;
 
+  const customeRenderers = {
+    paragraph(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].type === 'image') {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              height={300}
+              width={600}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const { language, value } = code;
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={value}
+        />
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={title} image={imagePath} />
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown components={customeRenderers}>{content}</ReactMarkdown>
     </article>
   );
 }
--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,8 @@
+import ContactForm from "../components/contact/contact-form";
 
 export default function ContactPage() {
   return (
-    <div>ContactPage</div>
+    <ContactForm />
   )
 }
 
